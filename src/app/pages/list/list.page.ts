bc6beb1ea71de11940d5e872e22728e4303f0721
ALBERTO <canoalberto7@gmail.com>
2023-11-10 11:22:34 +0100

    Initial commit

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {LocationFull} from "../../common/interfaces";
+import {RickMortyService} from "../../services/rick-morty.service";
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
 })
-export class ListPage implements OnInit {
-
-  constructor() { }
+export class ListPage implements OnInit {locations: LocationFull[] = []
+  constructor(private service: RickMortyService) { }
 
   ngOnInit() {
+    this.loadLocations();
   }
 
+  private loadLocations() {
+    this.service.getLocations().subscribe(
+      {
+        next: value => {
+          this.locations = value.results;
+        },
+        error: err => {
+          console.error(err);
+        },
+        complete: () =>{
+          console.log('Done')
+        }
+      }
+    )
+  }
 }
